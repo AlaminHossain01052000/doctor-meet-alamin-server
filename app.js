4840fb2ce303407b4fbd586a3a19157eeeef1e8f
Mostofareza3 <86896410+Mostofareza3@users.noreply.github.com>
2022-04-27 13:45:49 +0600

    cors fix

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const errorMiddleware = require('./middleware/error')
 const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const fileUpload = require("express-fileupload");
 
 const app = express();
 
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(fileUpload());
+app.use(cors());
 
 //Route imports
 const product = require("./routes/productRoute");
