45eb83ee5e591c2742632ecbc79a6729cc76b13e
Pranta07 <palpranta@gmail.com>
2022-05-19 11:40:41 +0600

    report manage done

--- routes/doctorRoute.js
+++ routes/doctorRoute.js
@@ -1,7 +1,14 @@
-const express = require('express');
-const { getAllDoctors, addDoctor, getDoctorById, updateDoctor, deleteDoctor, getDoctorStats } = require('../controllers/doctorController');
+const express = require("express");
+const {
+    getAllDoctors,
+    addDoctor,
+    getDoctorById,
+    updateDoctor,
+    deleteDoctor,
+    getDoctorStats,
+    addReport,
+} = require("../controllers/doctorController");
 const router = express.Router();
-
 
 router.route("/doctors/all").get(getAllDoctors);
 
@@ -11,8 +18,8 @@ router.route("/doctors/statistics").get(getDoctorStats);
 
 router.route("/doctors/single/:id").get(getDoctorById);
 
-router.route("/doctors/:id")
-    .put(updateDoctor)
-    .delete(deleteDoctor)
+router.route("/doctors/:id").put(updateDoctor).delete(deleteDoctor);
+
+router.route("/report/:id").put(addReport);
 
 module.exports = router;
