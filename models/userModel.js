b0e54ab11895b00aec70dd23e46706fb131e9693
Pranta07 <palpranta@gmail.com>
2022-05-22 04:08:23 +0600

    api added

--- models/userModel.js
+++ models/userModel.js
@@ -4,65 +4,86 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const crypto = require("crypto");
 
-const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, "Please Enter Your Name"],
-        maxLength: [30, "Name cannot exceed 30 characters"],
-        minLength: [4, "Name should have more than 4 characters"],
-    },
-    email: {
-        type: String,
-        required: [true, "Please Enter Your Email"],
-        unique: true,
-        validate: [validator.isEmail, "Please Enter a valid Email"],
-    },
-    password: {
-        type: String,
-        required: [true, "Please Enter Your Password"],
-        minLength: [6, "Password should be greater than 8 characters"],
-    },
-    image: {
-        type: String,
-        default: "avatar"
-    },
-    // image: {
-    //     // public_id: String,
-    //     type: String,
-    //     required: [true, "Please Enter Your Avatar Link"]
-    //     // public_id: {
-    //     //     type: String,
-    //     //     required: true,
-    //     // },
-    //     // url: {
-    //     //     type: String,
-    //     //     required: true,
-    //     // },
-    // },
-    role: {
-        type: String,
-        default: "user",
-        enum: {
-            values: ["user", "doctor", "moderator", "admin"],
-            message: "User must required specified role.",
-        }
-    },
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, "Please Enter Your Name"],
+            maxLength: [30, "Name cannot exceed 30 characters"],
+            minLength: [4, "Name should have more than 4 characters"],
+        },
+        email: {
+            type: String,
+            required: [true, "Please Enter Your Email"],
+            unique: true,
+            validate: [validator.isEmail, "Please Enter a valid Email"],
+        },
+        password: {
+            type: String,
+            required: [true, "Please Enter Your Password"],
+            minLength: [6, "Password should be greater than 8 characters"],
+        },
+        image: {
+            type: String,
+            default: "avatar",
+        },
+        // image: {
+        //     // public_id: String,
+        //     type: String,
+        //     required: [true, "Please Enter Your Avatar Link"]
+        //     // public_id: {
+        //     //     type: String,
+        //     //     required: true,
+        //     // },
+        //     // url: {
+        //     //     type: String,
+        //     //     required: true,
+        //     // },
+        // },
+        role: {
+            type: String,
+            default: "user",
+            enum: {
+                values: ["user", "doctor", "moderator", "admin"],
+                message: "User must required specified role.",
+            },
+        },
 
-    resetPasswordToken: String,
-    resetPasswordExpire: Date,
-}, {
-    timestamps: true,
-    // toJSON: {
-    //     transform: (doc, ret) => {
-    //         delete ret.password;
-    //         delete ret.__v;
-    //         return ret;
-    //     }
-    // }
-});
+        resetPasswordToken: String,
+        resetPasswordExpire: Date,
+        reports: [
+            {
+                file: {
+                    type: String,
+                },
+                patientId: {
+                    type: String,
+                },
+                desc: {
+                    type: String,
+                },
+                review: {
+                    type: String,
+                },
+                status: {
+                    type: Boolean,
+                },
+            },
+        ],
+    },
+    {
+        timestamps: true,
+        // toJSON: {
+        //     transform: (doc, ret) => {
+        //         delete ret.password;
+        //         delete ret.__v;
+        //         return ret;
+        //     }
+        // }
+    }
+);
 
 userSchema.pre("save", async function (next) {
-
     /* in-case user don't change his password so hashed password will be again hashed to prevent this case we can use a condition*/
     if (!this.isModified("password")) {
         next();
@@ -74,16 +95,14 @@ userSchema.pre("save", async function (next) {
 userSchema.methods.getJWTToken = function () {
     // letter we will use this id for getProducts and etc. actions.
     return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_EXPIRE
-    })
+        expiresIn: process.env.JWT_EXPIRE,
+    });
 };
 
 //compare password
 userSchema.methods.comparePassword = async function (password) {
-
     return await bcrypt.compare(password, this.password);
-
-}
+};
 
 // Generating Password Reset Token
 userSchema.methods.getResetPasswordToken = function () {
@@ -100,7 +119,5 @@ userSchema.methods.getResetPasswordToken = function () {
 
     return resetToken;
 };
-
-
 
 module.exports = mongoose.model("User", userSchema);
