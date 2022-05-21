9e7b4f67938a622a429c978bcd12d4a033543d45
mahim-shariar <mdmahim924214@gmail.co>
2022-05-22 04:32:59 +0600

    done poling

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -8,38 +8,52 @@ const ErrorHandler = require("../utils/errorHandler");
 
 // GET All by donor
 const allArticles = catchAsyncError(async (req, res, next) => {
-    try {
-        const data = await Articles.find({}).sort({ rating: -1 });
-        const total = await Articles.find({}).count();
-        res.status(200).json({
-            result: data,
-            total: total,
-            message: "Success",
-        });
-    } catch (err) {
-        res.status(500).json({
-            error: "Not found any results.",
-        });
-    }
+  try {
+    const data = await Articles.find({}).sort({ rating: -1 });
+    const total = await Articles.find({}).count();
+    res.status(200).json({
+      result: data,
+      total: total,
+      message: "Success",
+    });
+  } catch (err) {
+    res.status(500).json({
+      error: "Not found any results.",
+    });
+  }
+});
+
+const getSingleArticle = catchAsyncError(async (req, res, next) => {
+  const Article = await Articles.find({ _id: req.params.id });
+
+  if (!Article) {
+    return next(new ErrorHandler("Articles not found with this Id", 404));
+  }
+
+  res.status(200).json({
+    success: true,
+    Article,
+  });
 });
 
 // POST A Review
 const addArticle = catchAsyncError(async (req, res, next) => {
-    const newArticle = new Articles(req.body);
-    newArticle.save((err) => {
-        if (err) {
-            res.status(500).json({
-                error: "There was a server side error!",
-            });
-        } else {
-            res.status(200).json({
-                message: "Article was inserted successfully!",
-            });
-        }
-    });
+  const newArticle = new Articles(req.body);
+  newArticle.save((err) => {
+    if (err) {
+      res.status(500).json({
+        error: "There was a server side error!",
+      });
+    } else {
+      res.status(200).json({
+        message: "Article was inserted successfully!",
+      });
+    }
+  });
 });
 
 module.exports = {
-    addArticle,
-    allArticles,
+  addArticle,
+  allArticles,
+  getSingleArticle,
 };
--- routes/articleRoute.js
+++ routes/articleRoute.js
@@ -1,8 +1,11 @@
 const express = require("express");
-const { addArticle, allArticles } = require("../controllers/articleController");
+const { addArticle, allArticles,getSingleArticle } = require("../controllers/articleController");
+
 const router = express.Router();
 
 router.route("/article").get(allArticles);
 router.route("/article/add").post(addArticle);
+router.route("/article/:id").get(getSingleArticle);
+
 
 module.exports = router;
