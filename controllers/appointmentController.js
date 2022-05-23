7e34eea4ab29dc05ab03f86a2bc31752055b605e
Pranta07 <palpranta@gmail.com>
2022-05-23 23:01:56 +0600

    appointment done

--- controllers/appointmentController.js
+++ controllers/appointmentController.js
@@ -2,131 +2,171 @@ const express = require("express");
 const mongoose = require("mongoose");
 const router = express.Router();
 const catchAsyncError = require("../middleware/catchAsyncError");
-const Appointment = require("../models/appointmentModel");
+const AppointmentsCollection = require("../models/appointmentModel");
 const ApiFeatures = require("../utils/apiFeatures");
 const ErrorHandler = require("../utils/errorHandler");
 
 // GET all appointment
 
 const getAllAppointment = catchAsyncError(async (req, res, next) => {
-  Appointment.find({})
-    .select({
-      _id: 0,
-      __v: 0,
-      date: 0,
-    })
-    .exec((err, data) => {
-      if (err) {
-        res.status(500).json({
-          error: "There was a server side error!",
+    AppointmentsCollection.find({})
+        .select({
+            __v: 0,
+            date: 0,
+        })
+        .exec((err, data) => {
+            if (err) {
+                res.status(500).json({
+                    error: "There was a server side error!",
+                });
+            } else {
+                res.status(200).json({
+                    result: data,
+                    message: "Success",
+                });
+            }
         });
-      } else {
-        res.status(200).json({
-          result: data,
-          message: "Success",
+});
+
+// user-appointments
+const getMyAppointments = catchAsyncError(async (req, res, next) => {
+    AppointmentsCollection.find({ patientEmail: req.params.email })
+        .select({
+            __v: 0,
+            date: 0,
+        })
+        .exec((err, data) => {
+            if (err) {
+                res.status(500).json({
+                    error: "There was a server side error!",
+                });
+            } else {
+                res.status(200).json({
+                    result: data,
+                    message: "Success",
+                });
+            }
         });
-      }
+});
+
+// doctor-appointments
+const getDoctorAppointments = catchAsyncError(async (req, res, next) => {
+    // console.log(req.params.email);
+    const query = { "doctorInfo.email": req.params.email };
+    AppointmentsCollection.find(query).exec((err, data) => {
+        if (err) {
+            res.status(500).json({
+                error: "There was a server side error!",
+            });
+        } else {
+            res.status(200).json({
+                result: data,
+                message: "Success",
+            });
+        }
     });
 });
 
 // GET A appointment by ID
 
 const getAppointmentById = catchAsyncError(async (req, res, next) => {
-  try {
-    const data = await Appointment.find({ _id: req.params.id });
-    res.status(200).json({
-      success: true,
-      data,
-    });
-  } catch (err) {
-    res.status(500).json({
-      error: "There was a server side error!",
-    });
-  }
+    try {
+        const data = await AppointmentsCollection.find({ _id: req.params.id });
+        res.status(200).json({
+            success: true,
+            data,
+        });
+    } catch (err) {
+        res.status(500).json({
+            error: "There was a server side error!",
+        });
+    }
 });
 
 // POST An appointment
 
 const addAnAppointment = catchAsyncError(async (req, res, next) => {
-  const newAppointment = new Appointment(req.body);
-  newAppointment.save((err) => {
-    if (err) {
-      res.status(500).json({
-        error: "There was a server side error!",
-      });
-    } else {
-      res.status(200).json({
-        message: "Appointment was inserted successfully!",
-      });
-    }
-  });
+    const newAppointment = new AppointmentsCollection(req.body);
+    newAppointment.save((err) => {
+        if (err) {
+            res.status(500).json({
+                error: "There was a server side error!",
+            });
+        } else {
+            res.status(200).json({
+                message: "Appointment was inserted successfully!",
+            });
+        }
+    });
 });
 
 // POST MULTIPLE Appointment
 
 const addMultipleAppointment = catchAsyncError(async (req, res, next) => {
-  Appointment.insertMany(req.body, (err) => {
-    if (err) {
-      res.status(500).json({
-        error: "There was a server side error!",
-      });
-    } else {
-      res.status(200).json({
-        message: "Appointment were inserted successfully!",
-      });
-    }
-  });
+    AppointmentsCollection.insertMany(req.body, (err) => {
+        if (err) {
+            res.status(500).json({
+                error: "There was a server side error!",
+            });
+        } else {
+            res.status(200).json({
+                message: "Appointment were inserted successfully!",
+            });
+        }
+    });
 });
 
 // PUT Appointment
 
 const updateAppointment = catchAsyncError(async (req, res, next) => {
-  const result = Appointment.findByIdAndUpdate(
-    { _id: req.params.id },
-    {
-      $set: {
-        status: "active",
-      },
-    },
-    {
-      new: true,
-      useFindAndModify: false,
-    },
-    (err) => {
-      if (err) {
-        res.status(500).json({
-          error: "There was a server side error!",
-        });
-      } else {
-        res.status(200).json({
-          message: "Appointment was updated successfully!",
-        });
-      }
-    }
-  );
+    const result = AppointmentsCollection.findByIdAndUpdate(
+        { _id: req.params.id },
+        {
+            $set: {
+                status: "active",
+            },
+        },
+        {
+            new: true,
+            useFindAndModify: false,
+        },
+        (err) => {
+            if (err) {
+                res.status(500).json({
+                    error: "There was a server side error!",
+                });
+            } else {
+                res.status(200).json({
+                    message: "Appointment was updated successfully!",
+                });
+            }
+        }
+    );
 });
 
 // DELETE Appointment
 
 const deleteAppointment = catchAsyncError(async (req, res, next) => {
-  Appointment.deleteOne({ _id: req.params.id }, (err) => {
-    if (err) {
-      res.status(500).json({
-        error: "There was a server side error!",
-      });
-    } else {
-      res.status(200).json({
-        message: "Appointment was deleted successfully!",
-      });
-    }
-  });
+    AppointmentsCollection.deleteOne({ _id: req.params.id }, (err) => {
+        if (err) {
+            res.status(500).json({
+                error: "There was a server side error!",
+            });
+        } else {
+            res.status(200).json({
+                message: "Appointment was deleted successfully!",
+            });
+        }
+    });
 });
 
 module.exports = {
-  getAllAppointment,
-  getAppointmentById,
-  updateAppointment,
-  deleteAppointment,
-  addAnAppointment,
-  addMultipleAppointment,
+    getAllAppointment,
+    getAppointmentById,
+    updateAppointment,
+    deleteAppointment,
+    addAnAppointment,
+    addMultipleAppointment,
+    getMyAppointments,
+    getDoctorAppointments,
 };
--- models/appointmentModel.js
+++ models/appointmentModel.js
@@ -1,80 +1,83 @@
 const mongoose = require("mongoose");
 
 const appointmentSchema = mongoose.Schema(
-  {
-    patientName: {
-      type: String,
-      required: true,
+    {
+        patientName: {
+            type: String,
+            required: true,
+        },
+        patientEmail: {
+            type: String,
+            required: true,
+        },
+        age: {
+            type: String,
+            required: true,
+        },
+        gender: {
+            type: String,
+            required: true,
+        },
+        bloodGroup: {
+            type: String,
+            required: true,
+        },
+        weight: {
+            type: String,
+            required: true,
+        },
+        height: {
+            type: String,
+            required: true,
+        },
+        healthIssues: {
+            type: String,
+            required: true,
+        },
+        appointmentTime: {
+            type: Object,
+            required: true,
+        },
+        appointmentDate: {
+            type: Object,
+            required: true,
+        },
+        payment: {
+            type: Boolean,
+        },
+        doctorInfo: {
+            id: String,
+            name: {
+                type: String,
+                required: true,
+            },
+            username: String,
+            email: {
+                type: String,
+                required: true,
+            },
+            img: {
+                type: String,
+                required: true,
+            },
+            specialist: {
+                type: String,
+                required: true,
+            },
+            timeSlot: {
+                type: Array,
+                required: true,
+            },
+            visit: {
+                type: String,
+                required: true,
+            },
+        },
+        status: {
+            type: String,
+        },
     },
-    patientEmail: {
-      type: String,
-      required: true,
-    },
-    age: {
-      type: String,
-      required: true,
-    },
-    gender: {
-      type: String,
-      required: true,
-    },
-    bloodGroup: {
-      type: String,
-      required: true,
-    },
-    weight: {
-      type: String,
-      required: true,
-    },
-    height: {
-      type: String,
-      required: true,
-    },
-    healthIssues: {
-      type: String,
-      required: true,
-    },
-    appointmentTime: {
-      type: Object,
-      required: true,
-    },
-    appointmentDate: {
-      type: Object,
-      required: true,
-    },
-    doctorInfo: {
-      id: String,
-      name: {
-        type: String,
-        required: true,
-      },
-      username: String,
-      email: {
-        type: String,
-        required: true,
-      },
-      img: {
-        type: String,
-        required: true,
-      },
-      specialist: {
-        type: String,
-        required: true,
-      },
-      timeSlot: {
-        type: Array,
-        required: true,
-      },
-      visit: {
-        type: String,
-        required: true,
-      },
-    },
-    status: {
-      type: String,
-    },
-  },
-  { timestamps: true }
+    { timestamps: true }
 );
 
 module.exports = mongoose.model("Appointments", appointmentSchema);
