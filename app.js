cb6956b11b703f7ef1027feb9b3cd049749ba123
mhasancy <mhasancy@gmail.com>
2022-05-25 08:59:54 +0600

    try to deploy

--- app.js
+++ app.js
@@ -25,7 +25,6 @@ const doctor = require("./routes/doctorRoute");
 const review = require("./routes/reviewRoute");
 const report = require("./routes/reportRoute");
 const article = require("./routes/articleRoute");
-
 // const payment = require("./routes/paymentRoute");
 
 app.use("/api/v1", product);
@@ -41,6 +40,7 @@ app.use("/api/v1", article);
 
 // error handler middleware
 app.use(errorMiddleware);
+
 app.get("/", (req, res) => res.send("hello from server"));
 
 module.exports = app;
